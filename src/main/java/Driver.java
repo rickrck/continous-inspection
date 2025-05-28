431199e69c45fcd5378344860e93f5c798b13b6e
Ricky Renaldi <109163100+rickrck@users.noreply.github.com>
2025-05-28 23:18:17 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -8,14 +8,15 @@ public class Driver {
 		
 		System.out.println("Current count:" + counter.getCount());
 		
-		counter.increment();
+		counter.increment(5);
 		
 		System.out.println("Current count:" + counter.getCount());
 		
-		counter.decrement();
+		counter.decrement(2);
 		
 		System.out.println("Current count:" + counter.getCount());
-		
+
+		counter.increment(5);
 	}
 
 }
